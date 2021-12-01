7cae595d8e7ddb664b1729efda8e775f7080470b
ronald.taylor.stl@gmail.com <ronald.taylor.stl@gmail.com>
2021-12-01 12:23:28 -0600

    changed name

---
+++ secondhour/main1.js
@@ -0,0 +1,10 @@
+//declare Variable
+
+//var age ='34'; Var is not used anymore
+let age ='34';
+//Let allows you to change the value
+ const age ='26';
+ //const stays the same is constant 
+ age1 = 15;
+
+ console.log(age1);
