edf744a02a2ed2acb0cc8936c8e5816a5598bcda
ronald.taylor.stl@gmail.com <ronald.taylor.stl@gmail.com>
2021-12-01 12:42:02 -0600

    Learned how to combine variables

--- secondhour/main1.js
+++ secondhour/main1.js
@@ -8,3 +8,19 @@ let age ='34';
  age1 = 15;
 
  console.log(age1);
+
+ const skill1 = "Ronald's skills are improving with Elvis Help";
+
+ const skill2 = "Ronald's skills are improving with Elvis Help";
+
+ console log(skill2);
+
+ const firstName = 'Ronald';
+ const lastName = 'Taylor'
+ 
+ let fullName;
+
+ fullName = firstName + "" + lastName;
+
+ //Learned from Elvis to combine variables
+
