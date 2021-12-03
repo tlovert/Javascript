a374631a120c314ad14777c6cc5a4335dd6c7cca
ronald.taylor.stl@gmail.com <ronald.taylor.stl@gmail.com>
2021-12-03 12:20:39 -0600

    Added new info abot numbers

--- 3rd hour/main1.js
+++ 3rd hour/main1.js
@@ -24,3 +24,37 @@ let age ='34';
 
  //Learned from Elvis to combine variables
 
+//Numbers
+
+let age= 34;//integer
+let age2= 2.2;//Decimal
+//Basic math operations
+const add = age + age2;
+const sub = age - age2;
+const mult = age * age2;
+const div = age / age2;
+
+console.assert(sub);
+
+let num - 40;
+num += 4;//Increase by 4
+num -1; 1//Decrease by 1
+num++;//Increase by 1
+num--;//Decrease by 1
+console.log(num);
+
+let pizza = 10;
+let children = 3;
+
+const slices = pizza %  children;
+
+
+console.log("I will have" + slices + "for myself")
+
+
+let num1 = 24
+
+let num = 16;
+
+let num3 = num1 +num2;
+console.log(num3):
